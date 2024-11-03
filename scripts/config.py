ac79161a2a607ae8c91d66c3159c7b9957da64ee
Filippo <filippo99merlo@gmail.com>
2024-11-03 17:45:04 +0100

    reg

--- scripts/config.py
+++ scripts/config.py
@@ -8,5 +8,3 @@ dataset_path = os.path.join(data_dir_path, 'final_dataset.json')
 # Path of the folder with the images
 images_path = os.path.join(data_dir_path, 'generated_images')
 new_images_path = os.path.join(data_dir_path, 'resized_images')
-# Ensure the output directory exists
-os.makedirs(os.path.dirname(new_images_path), exist_ok=True)
