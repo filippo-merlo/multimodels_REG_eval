10075ee58b14ce27dc8e8a006d4406fe2d8b259d
Filippo <filippo99merlo@gmail.com>
2024-11-04 14:50:48 +0100

    srg

--- scripts/config.py
+++ scripts/config.py
@@ -3,8 +3,7 @@ import os
 data_dir_path = '/mnt/cimec-storage6/users/filippo.merlo/sceneREG_data'
 
 # Path of the dictionary with the data 
-dataset_path = os.path.join(data_dir_path, 'final_dataset.json')
+dataset_path = os.path.join(data_dir_path, 'final_dataset_resized.json')
 
 # Path of the folder with the images
-images_path = os.path.join(data_dir_path, 'generated_images')
-new_images_path = os.path.join(data_dir_path, 'resized_images')
+images_path = os.path.join(data_dir_path, 'resized_images')
--- scripts/preprocess_data.py
+++ scripts/preprocess_data.py
@@ -1,23 +1,50 @@
 import os
 import json
-from config import *
+from config import dataset_path, images_path
 
-def load_data():
+def load_dataset():
     # Load and preprocess data
-    data_path = "data/processed/benchmark_data.json"
-    if not os.path.exists(data_path):
-        preprocess_data()
-    with open(data_path, 'r') as f:
+    with open(dataset_path, 'r') as f:
         return json.load(f)
 
-def preprocess_data():
-    # Placeholder for actual data preprocessing
-    raw_data_path = "data/raw/raw_data.json"
-    processed_data_path = "data/processed/benchmark_data.json"
-    with open(raw_data_path, 'r') as f:
-        raw_data = json.load(f)
+def get_images():
+    # Load and preprocess images
+    images = {}
+    for filename in os.listdir(images_path):
+        if filename.endswith('.jpg'):
+            images[filename] = os.path.join(images_path, filename)
+    return images
+
+### CHECK BBOXES
+import cv2
+save_path = os.path.join('/mnt/cimec-storage6/users/filippo.merlo/sceneREG_data', 'check_bbox')
+def save_with_box(image_path, bbox, save_path):
+    # load image
+    image = cv2.imread(image_path)
     
-    processed_data = raw_data  # Apply processing here
-    with open(processed_data_path, 'w') as f:
-        json.dump(processed_data, f)
+    # draw box
+    x, w, y, h = bbox
+    cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)
     
+    # save image
+    cv2.imwrite(save_path, image)
+
+dataset = load_dataset()
+images = get_images()
+for image_name, image_path in images.items():
+    save_with_box(image_path, dataset[image_name]['target_bbox'], os.path.join(save_path, image_name))
+
+
+
+
+### ADD NOISE
+from PIL import Image
+import numpy as np 
+def add_gaussian_noise_in_bbox(image_path, bbox, noise_level=0.0):
+    # Add noise to the image within the bounding box
+    image = Image.open(image_path)
+    image_np = np.array(image)
+    # box notation [x,w,h,y]
+    x, w, h, y = bbox
+
+    return image
