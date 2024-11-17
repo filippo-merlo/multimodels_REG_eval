e19fbe7297382b5791349766d974283ae8ab2420
Filippo <filippo99merlo@gmail.com>
2024-11-17 16:21:16 +0100

    jh

--- scripts/models_scripts.py
+++ scripts/models_scripts.py
@@ -200,8 +200,30 @@ def load_model(model_name, device, model_dir, cache_dir):
 
         return model, generate
     
-    if model_name == '':
-        
+    elif model_name == "Qwen/Qwen-VL":
+        from transformers import AutoModelForCausalLM, AutoTokenizer
+        from transformers.generation import GenerationConfig
+        import torch
+        torch.manual_seed(1234)
+
+        tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen-VL", trust_remote_code=True)
+        model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen-VL", device_map="cuda", trust_remote_code=True).eval()
+
+        # Specify hyperparameters for generation (No need to do this if you are using transformers>=4.32.0)
+        # model.generation_config = GenerationConfig.from_pretrained("Qwen/Qwen-VL", trust_remote_code=True)
+
+        query = tokenizer.from_list_format([
+            {'image': 'https://qianwen-res.oss-cn-beijing.aliyuncs.com/Qwen-VL/assets/demo.jpeg'},
+            {'text': 'Generate the caption in English with grounding:'},
+        ])
+        inputs = tokenizer(query, return_tensors='pt')
+        inputs = inputs.to(model.device)
+        pred = model.generate(**inputs)
+        response = tokenizer.decode(pred.cpu()[0], skip_special_tokens=False)
+        print(response)
+        # <img>https://qianwen-res.oss-cn-beijing.aliyuncs.com/Qwen-VL/assets/demo.jpeg</img>Generate the caption in English with grounding:<ref> Woman</ref><box>(451,379),(731,806)</box> and<ref> her dog</ref><box>(219,424),(576,896)</box> playing on the beach<|endoftext|>
+        image = tokenizer.draw_bbox_on_latest_picture(response)
+       
     else:
         # other models
         return model, generate
