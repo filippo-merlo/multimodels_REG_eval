4066e1089dbb928d4df5013ab3753ce7bed3c4ec
Filippo <filippo99merlo@gmail.com>
2024-11-24 16:41:22 +0100

    wegf

--- run_all_evaluations.py
+++ run_all_evaluations.py
@@ -2,7 +2,7 @@
 import subprocess
 from scripts.manage_gpu import free_gpu_memory
 from scripts.data import load_dataset, get_images_names_path
-from scripts.config import model_list
+from scripts.config import *
 
 
 device = "cuda:0"  # Specify GPU device
@@ -10,8 +10,8 @@ device = "cuda:0"  # Specify GPU device
 for model_name in model_list:
     print(f"Evaluating {model_name}...")
     # Load data
-    data = load_dataset()
-    images_n_p = get_images_names_path()
+    data = load_dataset(dataset_path)
+    images_n_p = get_images_names_path(images_path)
 
     # Run evaluation as a subprocess to manage memory usage
     for model_name in model_list:
