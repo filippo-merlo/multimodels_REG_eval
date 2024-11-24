d660c144e416f2e756014f466ba2afbd82689162
Filippo <filippo99merlo@gmail.com>
2024-11-24 16:46:07 +0100

    weg

--- run_all_evaluations.py
+++ run_all_evaluations.py
@@ -2,7 +2,17 @@
 import subprocess
 from scripts.manage_gpu import free_gpu_memory
 from scripts.data import load_dataset, get_images_names_path
-from scripts.config import *
+
+
+# model list 
+model_list = [
+    'Salesforce/xgen-mm-phi3-mini-instruct-singleimg-r-v1.5',
+    'Salesforce/xgen-mm-phi3-mini-instruct-r-v1',
+    'microsoft/kosmos-2-patch14-224',
+    'cyan2k/molmo-7B-D-bnb-4bit',
+    'Qwen/Qwen2-VL-2B-Instruct-GPTQ-Int8',
+    'llava-hf/llava-onevision-qwen2-0.5b-si-hf',
+]
 
 
 device = "cuda:0"  # Specify GPU device
@@ -10,8 +20,6 @@ device = "cuda:0"  # Specify GPU device
 for model_name in model_list:
     print(f"Evaluating {model_name}...")
     # Load data
-    data = load_dataset(dataset_path)
-    images_n_p = get_images_names_path(images_path)
 
     # Run evaluation as a subprocess to manage memory usage
     for model_name in model_list:
--- scripts/config.py
+++ scripts/config.py
@@ -11,13 +11,3 @@ images_path = os.path.join(data_dir_path, 'resized_images')
 
 model_dir = '/mnt/cimec-storage6/shared/hf_lvlms'
 cache_dir = '/mnt/cimec-storage6/users/filippo.merlo/cache_regScene_eval'
-
-# model list 
-model_list = [
-    'Salesforce/xgen-mm-phi3-mini-instruct-singleimg-r-v1.5',
-    'Salesforce/xgen-mm-phi3-mini-instruct-r-v1',
-    'microsoft/kosmos-2-patch14-224',
-    'cyan2k/molmo-7B-D-bnb-4bit',
-    'Qwen/Qwen2-VL-2B-Instruct-GPTQ-Int8',
-    'llava-hf/llava-onevision-qwen2-0.5b-si-hf',
-]
