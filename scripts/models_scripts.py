ca6b90f831607cd6e894178f99fba8c4cb9ee996
Filippo <filippo99merlo@gmail.com>
2024-12-02 16:10:33 +0100

    xzdv

--- scripts/models_scripts.py
+++ scripts/models_scripts.py
@@ -202,7 +202,7 @@ def load_model(model_name, device, model_dir, cache_dir):
 
             # Move inputs to the correct device and create a batch of size 1
             inputs = {k: v.to(model.device).unsqueeze(0) for k, v in inputs.items()}
-            decoded_input = processor.decode(inputs['input_ids'][0])
+            decoded_input = processor.tokenizer.decode(inputs['input_ids'][0])
 
             # Generate output with configured generation settings
             with torch.autocast("cuda", enabled=True, dtype=torch.bfloat16):
