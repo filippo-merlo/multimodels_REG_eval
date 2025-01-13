50beed207f81e32fa6676645c6d169cc55367d3c
Filippo <filippo99merlo@gmail.com>
2025-01-13 13:51:53 +0100

    wrbe

--- data_analysis/llava_ov_attn_eval.py
+++ data_analysis/llava_ov_attn_eval.py
@@ -305,6 +305,7 @@ def show_mask_on_image(img, mask):
 pretrained = "lmms-lab/llava-onevision-qwen2-0.5b-si"
 model_name = "llava_qwen"
 device = "cuda:1"
+cache_dir = '/mnt/cimec-storage6/shared/hf_lvlms'
 #device_map = "auto"
 
 # load the model
@@ -318,7 +319,7 @@ llava_model_args = {
     "load_4bit" : load_4bit,
 }
 
-tokenizer, model, image_processor, max_length = load_pretrained_model(pretrained, None, model_name, **llava_model_args)
+tokenizer, model, image_processor, max_length = load_pretrained_model(pretrained, None, model_name, **llava_model_args, cache_dir=cache_dir)
 model.to(device)
 model.eval()
 
