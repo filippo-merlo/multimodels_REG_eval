1771335661b3c763c4dc0a728093e4a8c14620b8
filippo-merlo <filippo99merlo@gmail.com>
2025-05-12 18:25:44 +0200

    Update model name in evaluation script for consistency

--- run_all_evaluations.py
+++ run_all_evaluations.py
@@ -7,7 +7,7 @@ from scripts.data import load_dataset, get_images_names_path
 # model list 
 model_list = [
     'allenai/Molmo-7B-D-0924',
-    'Salesforce/xgen-mm-phi3-mini-instruct-singleimg-r-v1.5',
+    'Salesforce/xgen-mm-phi3-mini-instruct-interleave-r-v1.5',
     'microsoft/kosmos-2-patch14-224',
     'Qwen/Qwen2-VL-7B-Instruct',
     'llava-hf/llava-onevision-qwen2-0.5b-si-hf',
